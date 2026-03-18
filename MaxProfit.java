d82f237d5546346e7dcdd3a9271dadd50306e666
ShakthiKumar2030 <shakthikumar.b@prodian.co.in>
2026-03-18 15:19:40 +0530

    49th testcase handled in the Max profit problem

--- MaxProfit.java
+++ MaxProfit.java
@@ -1,52 +1,53 @@
+import java.util.*;
+
 public class MaxProfit {
 
     static int maxProfit;
-    static int bestT, bestP, bestC;
+    static List<int[]> bestSolutions;
 
     public static void main(String[] args) {
-
-        int[] testCases = {7, 8, 13};
-
+        int[] testCases = {7, 8, 13, 49};
         for (int n : testCases) {
-
             maxProfit = 0;
-            bestT = bestP = bestC = 0;
-
+            bestSolutions = new ArrayList<>();
             dfs(n, 0, 0, 0, 0, 0);
 
             System.out.println("Input Time Unit: " + n);
             System.out.println("Output Earnings: $" + maxProfit);
-            System.out.println("Solution:");
-            System.out.println("T: " + bestT + " P: " + bestP + " C: " + bestC);
+            System.out.println("Solutions");
+            for (int i = 0; i < bestSolutions.size(); i++) {
+                int[] sol = bestSolutions.get(i);
+                System.out.println((i+1) + ". T: " + sol[0] + " P: " + sol[1] + " C: " + sol[2]);
+            }
             System.out.println("--------------------------------");
         }
     }
 
     static void dfs(int n, int currentTime, int profit, int t, int p, int c) {
-
         if (profit > maxProfit) {
             maxProfit = profit;
-            bestT = t;
-            bestP = p;
-            bestC = c;
+            bestSolutions = new ArrayList<>();
+            bestSolutions.add(new int[]{t, p, c});
+        } else if (profit == maxProfit && profit > 0) {
+            int[] candidate = {t, p, c};
+            boolean duplicate = false;
+            for (int[] sol : bestSolutions) {
+                if (Arrays.equals(sol, candidate)) { duplicate = true; break; }
+            }
+            if (!duplicate) bestSolutions.add(candidate);
         }
 
-        if (currentTime + 5 <= n) {
-            int completionTime = currentTime + 5;
-            int earn = (n - completionTime) * 1500;
-            dfs(n, completionTime, profit + earn, t + 1, p, c);
+        if (currentTime + 5 < n) {
+            int ct = currentTime + 5;
+            dfs(n, ct, profit + (n - ct) * 1500, t + 1, p, c);
         }
-
-        if (currentTime + 4 <= n) {
-            int completionTime = currentTime + 4;
-            int earn = (n - completionTime) * 1000;
-            dfs(n, completionTime, profit + earn, t, p + 1, c);
+        if (currentTime + 4 < n) {
+            int ct = currentTime + 4;
+            dfs(n, ct, profit + (n - ct) * 1000, t, p + 1, c);
         }
-
-        if (currentTime + 10 <= n) {
-            int completionTime = currentTime + 10;
-            int earn = (n - completionTime) * 2000;
-            dfs(n, completionTime, profit + earn, t, p, c + 1);
+        if (currentTime + 10 < n) {
+            int ct = currentTime + 10;
+            dfs(n, ct, profit + (n - ct) * 2000, t, p, c + 1);
         }
     }
 }
